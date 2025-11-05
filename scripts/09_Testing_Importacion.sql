5c5ccf94f0e6792a511bec8b41a9fecd9e6559af
bianpedrol <bianpedrol@gmail.com>
2025-11-04 21:43:19 -0300

    cambios en reportes del 4 al 6

--- scripts/09_Testing_Importacion.sql
+++ scripts/09_Testing_Importacion.sql
@@ -36,12 +36,12 @@ GO
 
 -- Modificar estas rutas para que apunten a los archivos en tu PC
 DECLARE
-    @excelPath   NVARCHAR(4000) = N'C:\Users\cacer\OneDrive\Escritorio\TP Bases de Datos Aplicada\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\datos varios.xlsx',
-    @ufPath      NVARCHAR(4000) = N'C:\Users\cacer\OneDrive\Escritorio\TP Bases de Datos Aplicada\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\UF por consorcio.txt',
-    @personasCsv NVARCHAR(4000) = N'C:\Users\cacer\OneDrive\Escritorio\TP Bases de Datos Aplicada\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\Inquilino-propietarios-datos.csv',
-    @personasUF  NVARCHAR(4000) = N'C:\Users\cacer\OneDrive\Escritorio\TP Bases de Datos Aplicada\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\Inquilino-propietarios-UF.csv',
-    @pagos       NVARCHAR(4000) = N'C:\Users\cacer\OneDrive\Escritorio\TP Bases de Datos Aplicada\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\pagos_consorcios.csv',
-    @servicios   NVARCHAR(4000) = N'C:\Users\cacer\OneDrive\Escritorio\TP Bases de Datos Aplicada\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\Servicios.Servicios.json';
+    @excelPath   NVARCHAR(4000) = N'C:\Users\User\OneDrive - Universidad Nacional de la Matanza\Escritorio\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\datos varios.xlsx',
+    @ufPath      NVARCHAR(4000) = N'C:\Users\User\OneDrive - Universidad Nacional de la Matanza\Escritorio\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\UF por consorcio.txt',
+    @personasCsv NVARCHAR(4000) = N'C:\Users\User\OneDrive - Universidad Nacional de la Matanza\Escritorio\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\Inquilino-propietarios-datos.csv',
+    @personasUF  NVARCHAR(4000) = N'C:\Users\User\OneDrive - Universidad Nacional de la Matanza\Escritorio\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\Inquilino-propietarios-UF.csv',
+    @pagos       NVARCHAR(4000) = N'C:\Users\User\OneDrive - Universidad Nacional de la Matanza\Escritorio\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\pagos_consorcios.csv',
+    @servicios   NVARCHAR(4000) = N'C:\Users\User\OneDrive - Universidad Nacional de la Matanza\Escritorio\TP-Bases-de-Datos-Aplicada\archivos_origen\Archivos para el TP\Servicios.Servicios.json';
 
 PRINT '--- 2. Importando Consorcios...';
 EXEC Importacion.sp_importar_consorcios @ruta_archivo = @excelPath;
